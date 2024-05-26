92ad8b424b02b136a379021440bcf875254d33e7
eun <eunbyeol0708@gmail.com>
2024-05-27 05:15:39 +0700

    [EDIT] devices->tbdevices

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DevicesController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::get('/user', function (Request $request) { return $request->user();
+})->middleware('auth:sanctum');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/tbdevices', [DevicesController::class, 'index']);
+Route::post('/tbdevices', [DevicesController::class, 'store']);
+Route::get('/tbdevices/{id}', [DevicesController::class, 'show']);
+Route::put('/tbdevices/{id}', [DevicesController::class, 'update']);
+Route::delete('/tbdevices/{id}', [DevicesController::class, 'destroy']);
+Route::get('/test', function () {
+    return 'Hello world';
 });
